9a71de6890779c91f29a6e44d196f3f882117568
natalija <nmargaric@gmail.com>
2020-12-09 18:48:15 +0100

    commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const entryForm = document.getElementById("entryForm");
+const entriesSection = document.getElementById("entries");
+const entryTextBox = document.getElementsByClassName("entry-textbox");
+
+
 
 function addEntryToDom(event) {
     event.preventDefault();
+    const entryDiv = document.createElement("div");
+    entryDiv.className = "single-entry";
+    entryDiv.innerText = entryTextBox[0].value;
+    entriesSection.appendChild(entryDiv);
 }
 
 entryForm.addEventListener("submit", addEntryToDom)
+
 
